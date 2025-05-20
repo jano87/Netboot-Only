61dd89e053ba172af5718be3c4b14c0dda9efaa6
jano87 <33374981+jano87@users.noreply.github.com>
2025-05-20 04:08:15 -0400

    Update download.sh

--- ubuntu-netboot-downloader/download.sh
+++ ubuntu-netboot-downloader/download.sh
@@ -8,7 +8,7 @@ sudo mkdir -p "$TARGET_DIR"
 cd "$TARGET_DIR" || exit 1
 
 echo "🌐 Downloading Ubuntu netboot kernel and initrd..."
-sudo wget -nc http://archive.ubuntu.com/ubuntu/dists/jammy/main/installer-amd64/current/legacy-images/netboot/ubuntu-installer/amd64/linux
-sudo wget -nc http://archive.ubuntu.com/ubuntu/dists/jammy/main/installer-amd64/current/legacy-images/netboot/ubuntu-installer/amd64/initrd.gz
+sudo wget -nc http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/current/legacy-images/netboot/ubuntu-installer/amd64/linux
+sudo wget -nc http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/current/legacy-images/netboot/ubuntu-installer/amd64/initrd.gz
 
 echo "✅ Done. Netboot files are in: $TARGET_DIR"
